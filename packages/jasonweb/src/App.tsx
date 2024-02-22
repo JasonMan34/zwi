80b9fd059eb6aff981a82d2294bb35281d0a1f75
JasonMan34 <JasonErlMan@gmail.com>
2024-02-22 18:46:26 +0200

    mouse pos

--- packages/jasonweb/src/App.tsx
+++ packages/jasonweb/src/App.tsx
@@ -3,16 +3,33 @@ import './index.scss';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './features/Home';
+import useMousePosition from './hooks/useMousePosition';
+
+function GradientOnMouse() {
+  const mousePos = useMousePosition();
+
+  return (
+    <div
+      className="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute"
+      style={{
+        background: `radial-gradient(600px at ${mousePos.x}px ${mousePos.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
+      }}
+    ></div>
+  );
+}
 
 function App() {
   return (
-    <div className="mx-auto min-h-screen max-w-screen-xl px-6 md:px-12 lg:px-24">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
+    <>
+      <div className="mx-auto min-h-screen max-w-screen-xl px-6 md:px-12 lg:px-24 font-sans">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </div>
+      <GradientOnMouse />
+    </>
   );
 }
 
