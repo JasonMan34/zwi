bcf29f7148fc1b6ed6087de77ec35671db615e8c
Itamar Zwi <itamarzwi@gmail.com>
2025-01-22 14:20:58 +0200

    setup-mac for gal

--- packages/setup-mac/index.sh
+++ packages/setup-mac/index.sh
@@ -2,39 +2,42 @@
 set -o errexit
 set -o pipefail
 
-REPOS_DIR="repos"
-
 clear
-
-DEFAULT="\x1b[0m"
-BOLD="\x1b[1;37m"
 
 ohmyzsh_installation() {
   RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
 brew_installations() {
+  brew install awscli --force
+  brew install terraform --force
+  brew install thefuck --force
+
   brew install --cask iterm2 --force
   brew install --cask alt-tab --force
   brew install --cask rectangle --force
   brew install --cask maccy --force
-  brew install --cask postman --force
+  brew install --cask raycast --force
   brew install --cask docker --force
+  brew install --cask postman --force
   brew install --cask visual-studio-code --force
 }
 
 vscode_extensions_installations() {
   export NODE_OPTIONS="--no-deprecation"
-  code --install-extension dbaeumer.vscode-eslint           # ESLint
-  code --install-extension esbenp.prettier-vscode           # Prettier
-  code --install-extension Gruntfuggly.todo-tree            # Todo tree
-  code --install-extension wayou.vscode-todo-highlight      # Todo highlighter
-  code --install-extension eamodio.gitlens                  # GitLens
-  code --install-extension PKief.material-icon-theme        # Material Icon Theme
-  code --install-extension vscode-icons-team.vscode-icons   # VSCode Icon Theme
-  code --install-extension redhat.vscode-yaml               # YAML support
-  code --install-extension ms-azuretools.vscode-docker      # Dockerfile support
-  code --install-extension xabikos.JavaScriptSnippets       # JavaScript (ES6) snippet
+  code --install-extension Gruntfuggly.todo-tree                 # Todo tree
+  code --install-extension wayou.vscode-todo-highlight           # Todo highlighter
+  code --install-extension mechatroner.rainbow-csv               # Rainbow CSV
+  code --install-extension bmalehorn.shell-syntax                # Shell syntax support
+  code --install-extension foxundermoon.shell-format             # Shell formatter
+  code --install-extension timonwong.shellcheck                  # Shell linter
+  code --install-extension eamodio.gitlens                       # GitLens
+  code --install-extension PKief.material-icon-theme             # Material Icon Theme
+  code --install-extension redhat.vscode-yaml                    # YAML support
+  code --install-extension ms-azuretools.vscode-docker           # Dockerfile support
+  code --install-extension streetsidesoftware.code-spell-checker # Code spell checker
+  code --install-extension christian-kohler.path-intellisense    # Path intellisense
+  code --install-extension ms-python.python                      # Python
 }
 
 nvm_setup() {
