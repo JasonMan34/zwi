44a650358a35ddd8fe5a5b0b1d8fdb112bb5ec22
JasonMan34 <JasonErlMan@gmail.com>
2025-09-27 11:08:24 +0300

    Remove jason

--- packages/jasonweb/src/features/Home/index.tsx
+++ packages/jasonweb/src/features/Home/index.tsx
@@ -9,7 +9,7 @@ function Header() {
     <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24 pt-12">
       <div>
         <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
-          Itamar (Jason) Zwi
+          Itamar Zwi
         </h1>
         <h4 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
           Full Stack Developer
